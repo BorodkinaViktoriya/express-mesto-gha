47a04dee258a64325ef59e309490ccb84d81c761
Viktoriya Borodkina <Motiviruisya@yandex.ru>
2022-06-05 02:19:36 +0300

    feat: errors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cardRouter = require('./routes/cards');
 const {login, createUser} = require('./controllers/users');
 const auth = require('./middlewares/auth');
 
-const { PORT = 3000 } = process.env;
+const {PORT = 3000} = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mydb');
 
@@ -15,10 +15,20 @@ app.use(express.json());
 app.post('/signin', login);
 app.post('/signup', createUser);
 
-app.use(auth);
-app.use('/users', userRouter);
-app.use('/cards', cardRouter);
-app.use('*', (_, res) => res.status(404).send({ message: 'Страница не найдена' }));
+/* app.use(auth); */
+app.use('/users', auth, userRouter);
+app.use('/cards', auth, cardRouter);
+app.use('*', (_, res) => res.status(404).send({message: 'Страница не найдена'}));
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message} = err;
+  res.status(statusCode).send({
+    message: statusCode === 500
+      ? 'На сервере произошла ошибка'
+      : message,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
